19e033a031cf189c7c54b1c77188c7d0e8359beb
Kira Zholtikova <kzholtikova@kse.org.ua>
2023-12-11 11:37:44 +0200

    playing cycle in the main app
    - cycle to start new round till the user doesn't want to continue

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
 import game
+
+
+def get_valid_answer():
+    answer = input("Do you want a return match? ")
+    while answer.lower() not in ['y', 'n', 'yes', 'no']:
+        answer = input("Do you want a return match? ")
+    return answer.lower()
+
+
+def play():
+    playing = True
+    while playing:
+        print("3. 2. 1. GO!!!")
+        winner = game.play_round()
+        playing = get_valid_answer() in ['y', 'yes']
+
+    print("Bye!")
+
+
+play()
