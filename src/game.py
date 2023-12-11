bfa481a79b2d75f1559019e6264009607d40ce75
Kira Zholtikova <kzholtikova@kse.org.ua>
2023-12-11 11:26:51 +0200

    coordinates validaiton
    - are numbers
    - are within the range of field size
    - the cell isn't taken yet

--- src/game.py
+++ src/game.py
@@ -1,9 +1,29 @@
 FIRST_PLAYER = 'X'
 SECOND_PLAYER = 'O'
 
+is_valid_coordinate = lambda x: 0 <= x < 3
+coordinate_to_index = lambda x: int(x) - 1
 
-def input_move():
-    return row, column
+
+def is_cell_free(row, col, field):
+    return field[row][col] == '_'
+
+
+def input_move(field):
+    is_valid_move = False
+    while not is_valid_move:
+        try:
+            row, column = map(coordinate_to_index, input("Enter a row and a column to make a move: ").split(' '))
+            if not all(map(is_valid_coordinate, (row, column))):
+                print("Row and column values must be in range 1-3.")
+                continue
+            if not is_cell_free(row, column, field):
+                print("This cell is already taken! Choose another one.")
+                continue
+        except ValueError as e:
+            print("Invalid numbers.")
+        else:
+            return row, column
 
 
 def print_field():
