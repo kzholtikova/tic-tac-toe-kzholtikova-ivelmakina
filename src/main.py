2967c670f4792b56c351477101714ecfda7a4a67
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2023-12-13 18:28:47 +0200

    play() function
    -created function including ask_name(), welcome_message() and title_players() calling

--- src/main.py
+++ src/main.py
@@ -21,3 +21,12 @@ def title_players(winner_sign, player1, player2):
         leaderboard.Leaderboard().save_leaderboard(player2, player1, player2)
     else:
         leaderboard.Leaderboard().save_leaderboard(None, player1, player2)
+
+
+def play():
+    first_player = leaderboard.Player(ask_name("Please, enter first player's name: "))
+    second_player = leaderboard.Player(ask_name("Please, enter second player's name: "))
+    first_player.welcome_message()
+    second_player.welcome_message()
+
+    title_players(FIRST_PLAYER_SIGN, first_player, second_player)
