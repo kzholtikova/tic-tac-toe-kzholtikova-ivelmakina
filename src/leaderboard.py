4cd3d968c22eaa209e6b5d248f641e6a2a8f903a
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2023-12-13 17:52:51 +0200

    Player class
    -created Player class with properties

---
+++ src/leaderboard.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, username, victories=0, defeats=0, ties=0):
+        self.username = username
+        self.victories = victories
+        self.defeats = defeats
+        self.ties = ties
+
