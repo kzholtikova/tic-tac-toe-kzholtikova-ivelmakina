8d1e9227b45788b19d61d68fafc6ab9acdf8b9d3
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2023-12-13 18:02:02 +0200

    load_leaderboard() method
    -created method for reading leaderboard
    -created LEADERBOARD_PATH constant

--- src/leaderboard.py
+++ src/leaderboard.py
@@ -1,3 +1,6 @@
+LEADERBOARD_PATH = "leaderboard.tsv"
+
+
 class Player:
     def __init__(self, username, victories=0, defeats=0, ties=0):
         self.username = username
@@ -10,4 +13,13 @@ class Leaderboard:
     def __init__(self):
         self.players = []
 
-
+    def load_leaderboard(self):
+        try:
+            with open(LEADERBOARD_PATH, "r") as file:
+                self.players = []
+                for line in file:
+                    username, victories, defeats, ties = line.strip().split("\t")
+                    victories, defeats, ties = map(int, (victories, defeats, ties))
+                    self.players.append(Player(username, victories, defeats, ties))
+        except FileNotFoundError as e:
+            pass
