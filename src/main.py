1725e8a9b6e085f7876d0c4dd5a1a979ac998578
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2023-12-13 18:26:30 +0200

    title_players() function
    -created function for titling players depending on their sign

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+import leaderboard
+
+FIRST_PLAYER_SIGN = "X"
+SECOND_PLAYER_SIGN = "O"
+
 is_valid_name = lambda x: x.strip() != ""
 
 
@@ -7,3 +12,12 @@ def ask_name(input_text):
         print("Please, enter player's name.")
         name = input(input_text)
     return name
+
+
+def title_players(winner_sign, player1, player2):
+    if winner_sign == FIRST_PLAYER_SIGN:
+        leaderboard.Leaderboard().save_leaderboard(player1, player1, player2)
+    elif winner_sign == SECOND_PLAYER_SIGN:
+        leaderboard.Leaderboard().save_leaderboard(player2, player1, player2)
+    else:
+        leaderboard.Leaderboard().save_leaderboard(None, player1, player2)
